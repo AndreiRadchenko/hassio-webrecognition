506d9312c39720f8d08118a201e161992229fd13
AndreiRadchenko <d4100kx@gmail.com>
2022-03-25 00:24:50 +0200

    Delete webrecognition/data directory

--- webrecognition/data/TestPage_script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    const imgInp = document.getElementById("imgInp");
-    imgInp.onchange = evt => {
-        const [file] = imgInp.files;
-        if (file) {
-            blah.src = URL.createObjectURL(file);
-        };
-    };
-};
